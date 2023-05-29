92a574d901718a5855d78d707e75e911312fe2f6
yinmanman <62935215+yinmanm@users.noreply.github.com>
2023-05-29 15:39:14 +0800

    Delete .next directory

--- .next/static/chunks/app/chat/show/[id]/page-6bd08a4f6a4ba270.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[178,931,159],{7155:function(){}},function(n){n.O(0,[875,474,744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
--- .next/static/chunks/app/dashboard/page-31cdca2ffcfbf0fd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702],{9339:function(n,e,u){Promise.resolve().then(u.t.bind(u,414,23))}},function(n){n.O(0,[414,875,474,744],function(){return n(n.s=9339)}),_N_E=n.O()}]);
