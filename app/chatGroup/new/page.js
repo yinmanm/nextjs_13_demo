63b948554fcab543d7249de952c596af9a51e976
yinmanman <62935215+yinmanm@users.noreply.github.com>
2023-06-01 15:01:40 +0800

    review

--- app/chatGroup/new/page.js
+++ app/chatGroup/new/page.js
@@ -1,30 +1,12 @@
-import getBuyerListApi from '../../api/buyer/list';
 import ChatGroupForm from './_form';
 
 export default async function ChatGroupCreate() {
 
-  const buyerList = await getBuyerListApi();
-
-  async function search(searchString) {
-    "use server";
-    console.log('server', searchString)
-
-    return buyerList.filter((buyer) =>
-      buyer.email.toLowerCase().includes(searchString.toLowerCase())
-    )
-  }
-
-  if(buyerList.length > 0) {
-    return (
-      <div className='flex-1 flex flex-col py-5 md:py-14'>
-        <div className='flex-1 flex flex-col max-w-xl w-full mx-auto'>
-          <ChatGroupForm search={search} />
-        </div>
-      </div>
-    )
-  }
   return (
-    <div></div>
+    <div className='flex-1 flex flex-col py-5 md:py-14'>
+      <div className='flex-1 flex flex-col max-w-xl w-full mx-auto'>
+        <ChatGroupForm />
+      </div>
+    </div>
   )
-  
 }
